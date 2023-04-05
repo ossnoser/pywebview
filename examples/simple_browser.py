840c5d75b56d568dc5a2b3f284e0ed83b37efd03
Massimo Oss Noser <massimo.ossnoser@neuromoddevices.com>
2023-04-05 13:42:12 +0100

    fix(example): Modified examples/simple_browser.py to run the WebView2 runtime from custom directory
    Run the WebView2 runtime from the project directory

--- examples/simple_browser.py
+++ examples/simple_browser.py
@@ -1,3 +1,5 @@
+import os
+from pathlib import Path
 import webview
 
 """
@@ -6,5 +8,7 @@ This example demonstrates how to create a webview window.
 
 if __name__ == '__main__':
     # Create a standard webview window
+    exe_dir = Path(os.getcwd()).parent / "webview2"
     window = webview.create_window('Simple browser', 'https://pywebview.flowrl.com/hello')
-    webview.start()
+    webview.start(exe_dir=str(exe_dir))
+    
